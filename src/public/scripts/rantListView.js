261b94c9d3cf38879f99216386eeba5e530fde7a
Alberto González <albertoglezb@hotmail.com>
2021-03-22 07:20:51 -0600

    The 4th bug was solved.

--- src/public/scripts/rantListView.js
+++ src/public/scripts/rantListView.js
@@ -26,7 +26,12 @@ var RantView = Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html(this.template(this.model.toJSON()));
+    var rant = this.model.toJSON();
+    if(typeof rant.text !== 'string'){
+      console.error('Receive invalid rant data: ', rant)
+      rant.text = "Sorry! an error occurre. We're on it!"
+    }
+    this.$el.html(this.template(rant));
     return this;
   },
 
